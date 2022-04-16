315ed029362b4a7c056267a2a9f6cb2d51a3d5bb
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-04-15 22:05:47 -0500

    descomente una seccion

--- weekly_mission_2/examples1-operadores/example7.js
+++ weekly_mission_2/examples1-operadores/example7.js
@@ -1,15 +1,18 @@
 // Ejemplo 7: Uso de filter para filtrar una lista de elementos
-// const countries7 = ["Finland", "Denmark", "Sweden", "Norway", "Iceland"];
-// const countriesContainingLand = countries7.filter(
-//   (
-//     country // esta es una función
-//   ) => country.includes("land") // indicación para solo filtrar elementos que incluyan "land"
-// );
-// console.log("Ejemplo 7: Uso de filter para filtrar una lista de elementos");
-// console.log(countriesContainingLand);
-// const countriesEndsByia = countries.filter((country) => country.endsWith("ia"));
-// console.log("Ejemplo 7: Paises que terminan en i");
-// console.log(countriesEndsByia);
+const countries7 = ["Finland", "Denmark", "Sweden", "Norway", "Iceland"];
+const countriesContainingLand = countries7.filter(
+  (
+    country // esta es una función
+  ) => country.includes("land") // indicación para solo filtrar elementos que incluyan "land"
+);
+console.log("Ejemplo 7: Uso de filter para filtrar una lista de elementos");
+console.log(countriesContainingLand);
+
+const countriesEndsByia = countries7.filter((country) =>
+  country.endsWith("ia")
+);
+console.log("Ejemplo 7: Paises que terminan en i");
+console.log(countriesEndsByia);
 
 //EJERCICIO PROPIO
 
