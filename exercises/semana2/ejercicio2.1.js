00ff07a4e11524c2d60fd3f8469c819f017014cc
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-04-17 23:20:43 -0500

    se agrego class y constructor

---
+++ exercises/semana2/ejercicio2.1.js
@@ -0,0 +1,60 @@
+//POO
+
+//objeto de JS
+
+const woopa_obj = {
+  name: "woopa",
+  mission: "nodeJS",
+  age: "12",
+  color: "pink",
+};
+
+//CREAR UNA  CLASE
+
+class ajolonauta {
+  constructor(name, mission, age, color) {
+    this.name = name;
+    this.mission = mission;
+    this.age = age;
+    this.color = color;
+  }
+}
+
+console.log(ajolonauta);
+
+//Instanciar es crear un objeto a partir de una clase
+
+const woopa = new ajolonauta("woopa", "java", 17, "white");
+const woopy = new ajolonauta("woopy", "nodejs", 18, "blue");
+const wooper = new ajolonauta("wooper", "phyton", 21, "red");
+
+console.log(woopa);
+console.log(woopy);
+console.log(wooper);
+
+const woopa_obj_js = {
+  name: "robert",
+  degree: "software developer",
+  companyTarget: "EPAM",
+  school: "microsoft",
+};
+
+class student {
+  constructor(name, degree, companyTarget, school) {
+    this.name = name;
+    this.degree = degree;
+    this.companyTarget = companyTarget;
+    this.school = school;
+  }
+}
+
+const mexicanStudent = new student(
+  "Omar",
+  "software engineer",
+  "epam",
+  "wizeline"
+);
+
+console.log(mexicanStudent);
+
+console.log(woopa_obj_js);
