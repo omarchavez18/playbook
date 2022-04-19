431dd465e496e58df7fff320f302cd612b5be726
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-04-19 18:27:46 -0500

    se agregaron pruebas al test

--- weekly_mission_2/examples5/pokemon.test.js
+++ weekly_mission_2/examples5/pokemon.test.js
@@ -1,6 +1,14 @@
 import Pokemon from "./pokemon";
 
-test("1) Create a new object pokemon", () => {
-  const myPokemon = new Pokemon("Pikachu");
-  expect(myPokemon.name).toBe("Pikachussssssss"); // Corrige esta prueba
+test("1) Create a new object pokemon pikachu", () => {
+  const myPokemon = new Pokemon("Pikachu", "electric", 15);
+  expect(myPokemon.name).toBe("Pikachu");
+  expect(myPokemon.type).toBe("electric");
+  expect(myPokemon.age).toBe(15);
+});
+
+test("2) create a new object pokemon", () => {
+  const anotherPokemon = new Pokemon("bulbasaur", "grass");
+  expect(anotherPokemon.name).toBe("bulbasaur");
+  expect(anotherPokemon.type).toBe("grassa");
 });
