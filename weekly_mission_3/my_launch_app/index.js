389cbb8ff0eab3e67c38dad786d1cb4214146096
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-04-19 23:48:03 -0500

    creacion de index.js, importacion de archivo missioncommander y instanciar objetos

---
+++ weekly_mission_3/my_launch_app/index.js
@@ -0,0 +1,8 @@
+const MissionCommander = require("./app/missionCommander");
+
+const omar = new MissionCommander("Omar");
+const diego = new MissionCommander("diego");
+const nacho = new MissionCommander("Nacho");
+console.log(omar.name);
+console.log(diego.name);
+console.log(nacho.name);
