011b4e2db50cfbb7a57317eba97feadb29b91940
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-04-21 18:50:41 -0500

    se creo una segunda prueba añadiendo getters

--- weekly_mission_3/twitter/test/models/user.test.js
+++ weekly_mission_3/twitter/test/models/user.test.js
@@ -1,7 +1,7 @@
 const User = require("./../../app/models/user");
 
 describe("Unit test for User class", () => {
-  test("Create an User object", () => {
+  test("1) Create an User object", () => {
     const user = new User(18, "omarcgdv", "Omar", "bio");
     expect(user.id).toBe(18);
     expect(user.username).toBe("omarcgdv");
@@ -9,5 +9,16 @@ describe("Unit test for User class", () => {
     expect(user.bio).toBe("bio");
     expect(user.dateCreated).not.toBeUndefined();
     expect(user.lastUpdated).not.toBeUndefined();
+  });
+
+  // Requerimiento 3: Cada objeto tipo user necesita los siguietes gettes: `getUsername`, `getBio`, `getDateCreated`, `getLastUpdated`.
+  //Solución: Agrega los getters regresando la propiedad indicada, no olvides usar `this`.
+
+  test("2) Add getters", () => {
+    const user = new User(1, "danielcgdv", "Daniel", "Bio");
+    expect(user.getUsername).toBe("danielcgdv");
+    expect(user.getBio).toBe("Bio");
+    expect(user.getDateCreated).not.toBeUndefined();
+    expect(user.getLastUpdated).not.toBeUndefined();
   });
 });
