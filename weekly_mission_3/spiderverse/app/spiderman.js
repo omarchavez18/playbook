a2e636473baa5b234e84e14ff04c29cecc2af2f9
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-04-20 23:38:49 -0500

    creacion de class spiderman y se exporto

---
+++ weekly_mission_3/spiderverse/app/spiderman.js
@@ -0,0 +1,11 @@
+class spiderMan {
+  constructor(name, age, actor, moviesNumber, studio) {
+    this.name = name;
+    this.age = age;
+    this.actor = actor;
+    this.moviesNumber = moviesNumber;
+    this.studio = studio;
+  }
+}
+
+module.exports = spiderMan;
