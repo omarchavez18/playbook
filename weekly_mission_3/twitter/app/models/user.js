bb5046b01f88d260938c9eb9c22dc45ff73e8798
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-04-21 18:33:43 -0500

    se modificaron los valores "dateCreated" y "lastUpdated" por new Date() para entregar la fecha y que pase la prueba

--- weekly_mission_3/twitter/app/models/user.js
+++ weekly_mission_3/twitter/app/models/user.js
@@ -1,12 +1,15 @@
 //Requerimiento 1: Se necesita un modelo `User` para guardar la información de un usuario de nuestra app: `id`, `username`, `name`, `bio`, `dateCreated` y `lastUpdated`.
+// Requerimiento 2: Las propiedades `dateCreated` y `lastUpdated` deberán ser datos de tipo fecha que guarden el momento en que se instancie un nuevo objeto de la clase `User`.
+//Modifica la prueba de unidad creada anteriormente. Para ello modifica el código, ya no envíes los valores de dateCreated y lastUpdated al instancias la clase, y verifica en la prueba que a pesar de ya no enviarlos, no recibas un valor undefined
 class User {
   constructor(id, username, name, bio, dateCreated, lastUpdated) {
     this.id = id;
     this.username = username;
     this.name = name;
     this.bio = bio;
-    this.dateCreated = dateCreated;
-    this.lastUpdated = lastUpdated;
+    // estos 2 abajo nos devuelven las fechas
+    this.dateCreated = new Date();
+    this.lastUpdated = new Date();
   }
 }
 
