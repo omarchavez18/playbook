2f60774897e853a854d6cddf3a43a8ba2e9282a1
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-04-21 18:34:50 -0500

    se modifico la prueba del dateCreated y lastUpdated por .not.toBeUndefined()

--- weekly_mission_3/twitter/test/models/user.test.js
+++ weekly_mission_3/twitter/test/models/user.test.js
@@ -2,19 +2,12 @@ const User = require("./../../app/models/user");
 
 describe("Unit test for User class", () => {
   test("Create an User object", () => {
-    const user = new User(
-      18,
-      "omarcgdv",
-      "Omar",
-      "bio",
-      "dateCreated",
-      "lastUpdated"
-    );
+    const user = new User(18, "omarcgdv", "Omar", "bio");
     expect(user.id).toBe(18);
     expect(user.username).toBe("omarcgdv");
     expect(user.name).toBe("Omar");
     expect(user.bio).toBe("bio");
-    expect(user.dateCreated).toBe("dateCreated");
-    expect(user.lastUpdated).toBe("lastUpdated");
+    expect(user.dateCreated).not.toBeUndefined();
+    expect(user.lastUpdated).not.toBeUndefined();
   });
 });
