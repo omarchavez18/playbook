49412931281d89b4dda4438d4f0f5d5a2b2ffc66
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-04-21 18:26:23 -0500

    se creo el test de user

---
+++ weekly_mission_3/twitter/test/models/user.test.js
@@ -0,0 +1,20 @@
+const User = require("./../../app/models/user");
+
+describe("Unit test for User class", () => {
+  test("Create an User object", () => {
+    const user = new User(
+      18,
+      "omarcgdv",
+      "Omar",
+      "bio",
+      "dateCreated",
+      "lastUpdated"
+    );
+    expect(user.id).toBe(18);
+    expect(user.username).toBe("omarcgdv");
+    expect(user.name).toBe("Omar");
+    expect(user.bio).toBe("bio");
+    expect(user.dateCreated).toBe("dateCreated");
+    expect(user.lastUpdated).toBe("lastUpdated");
+  });
+});
