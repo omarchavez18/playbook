c128066723723e3df09b4cf95dfde4e06099f8be
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-04-22 23:10:39 -0500

    agregue nueva ruta "/launchx"

--- weekly_mission_3/express/app.js
+++ weekly_mission_3/express/app.js
@@ -3,9 +3,16 @@ const express = require("express");
 const app = express();
 const port = 3000; // localhost:3000
 
+//localhost:3000
 //req = request  res= response
 app.get("/", (req, res) => {
   res.send("hola a todos los compañeros explorers con todo|");
+});
+
+//localhost:3000/launchx
+//req = request  res= response
+app.get("/launchx", (req, res) => {
+  res.send("Bienvenidos a launchX");
 });
 
 app.listen(port, () => {
