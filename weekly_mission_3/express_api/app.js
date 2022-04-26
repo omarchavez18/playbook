fe90e50789b141386946c28c5fe8cb25b4eb0fa4
Omar <omarchavezgutierrezdevelasco@gmail.com>
2022-04-25 19:24:51 -0500

    creacion del metodo "post" , sirve para crear

--- weekly_mission_3/express_api/app.js
+++ weekly_mission_3/express_api/app.js
@@ -12,7 +12,7 @@ const port = 3000;
 // esto es un endpoint
 //routes
 //Methods HTTP: GET,POST, PUT, DELETE
-const explorer1 = { id: 1, name: "explorer1Omar" };
+const explorer1 = { id: 1, name: "explorer1Omr" };
 const explorer2 = { id: 2, name: "explorer2Chimo" };
 const explorer3 = { id: 3, name: "explorer3Pierre" };
 const explorers = [explorer1, explorer2, explorer3];
@@ -21,6 +21,20 @@ app.get("/v1/explorers", (req, res) => {
 
   //HTTP CODE STATUS: 200
   res.status(200).json(explorers);
+});
+
+app.get("/v1/explorers/:id", (req, res) => {
+  console.log(`GET BY ID explorers V1 API ${new Date()}`);
+  const explorer1 = { id: 1, name: "explorer18" };
+  //HTTP CODE STATUS: 200
+  res.status(200).json(explorer1);
+});
+
+app.post("/v1/explorers", (req, res) => {
+  console.log(`POST explorers V1 API ${new Date()}`);
+  //Agregar la logica para persistir;
+  console.log(req.body); // parametros de req
+  res.status(201).json({ message: "creado exitosamente" });
 });
 
 //asi inicializamos la app
